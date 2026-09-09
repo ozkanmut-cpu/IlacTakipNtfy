bf395f540d65baa436f97c7d8119b3ece9389351
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 15:05:19 +0300

    Run ntfy cursor tests under Robolectric

--- app/src/test/java/com/ozkanmut/ilactakip/NtfyBatchCursorTest.kt
+++ app/src/test/java/com/ozkanmut/ilactakip/NtfyBatchCursorTest.kt
@@ -4,7 +4,10 @@ import org.json.JSONObject
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class NtfyBatchCursorTest {
     @Test
     fun malformedEnvelope_doesNotAdvanceCursor() {
