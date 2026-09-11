8d921bdc7e8b1a033e2888a012c6ee232bd16c4c
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-11 05:29:16 +0300

    Bootstrap real iOS Dosefolk runtime

--- ios/Dosefolk/Dosefolk/DosefolkApp.swift
+++ ios/Dosefolk/Dosefolk/DosefolkApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct DosefolkApp: App {
+    private let runtime: DosefolkRuntime?
+    private let startupError: String?
+
+    init() {
+        do {
+            runtime = try DosefolkRuntime()
+            startupError = nil
+        } catch {
+            runtime = nil
+            startupError = error.localizedDescription
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(runtime: runtime, startupError: startupError)
         }
     }
 }
