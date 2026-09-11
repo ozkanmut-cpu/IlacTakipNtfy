26fe7ba1feb72c22b086f9a40e4fd22f89e15507
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-11 08:24:14 +0300

    Make PRN formatting test timezone-safe

--- ios/Dosefolk/DosefolkTests/PrnDoseServiceTests.swift
+++ ios/Dosefolk/DosefolkTests/PrnDoseServiceTests.swift
@@ -3,15 +3,15 @@ import XCTest
 
 final class PrnDoseServiceTests: XCTestCase {
     func testDateAndTimeFormattingIsProtocolStable() throws {
-        var components = DateComponents()
-        components.calendar = Calendar(identifier: .gregorian)
-        components.timeZone = TimeZone(secondsFromGMT: 0)
-        components.year = 2026
-        components.month = 9
-        components.day = 11
-        components.hour = 8
-        components.minute = 7
-        let date = try XCTUnwrap(components.date)
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = .current
+        let date = try XCTUnwrap(calendar.date(from: DateComponents(
+            year: 2026,
+            month: 9,
+            day: 11,
+            hour: 8,
+            minute: 7
+        )))
 
         XCTAssertEqual(PrnDoseService.dateString(date), "2026-09-11")
         XCTAssertEqual(PrnDoseService.timeString(date), "08:07")
